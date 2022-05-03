8346a5cb8a4df0821d6c10a316011b47ded95bff
wangrcwilson <104078776+wangrcwilson@users.noreply.github.com>
2022-05-03 12:20:52 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "5ef09a1e-ee13-41a0-885f-cb3a5f4026c6",
                         "alterId": 0
                     }
                 ],
